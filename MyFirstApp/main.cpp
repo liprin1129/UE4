815a55c8c4cd339f963224210ad406998ca8eb0c
Seongmuk Gang <liprin1129@gmail.com>
2016-09-19 14:40:36 +0100

    Initial Commit

---
+++ MyFirstApp/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  MyFirstApp
+//
+//  Created by Seongmuk Gang on 19/09/2016.
+//  Copyright © 2016 Seongmuk Gang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
